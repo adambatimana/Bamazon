737ee983e5b05d830c166c85aa5694ce37f35181
Adam Batimana <adambatimana@gmail.com>
2017-06-28 12:38:40 -0400

    select item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,49 +20,88 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if(err) throw err;
   console.log(`connected as id ${ connection.threadId }`);
-
-  connection.query("SELECT * FROM products", function(err,res){
-    if (err) throw err;
-          // PRINT ALL ITEMS
-           for (var i = 0; i < res.length; i++) {
-             console.log(
-               "ITEM #" + res[i].item_id,
-               "PRODUCT: " + res[i].product_name,
-               "PRICE: $" + res[i].price
-             );
-           }
-  })
+  displayItems();
 });
 
-// function start() {
-//   inquirer
-//     .prompt(
-//       {
-//       name: "idNo",
-//       type: "input",
-//       message: "Please insert ID number of product you would like to buy"
-//     },
-//     {
-//       name: "noOfItem",
-//       type: "input",
-//       message: "How many would you like to buy?"
-//     }
-//   )//end prompt
-//     .then(function(answer) {
-//         if (answer.idNo == "item" && answer.noOfItem > quantity of item ) {
-//
-//         }
-//
-//
-//
-//
-//       // // based on their answer, user, marketing manager or ?
-//       // if (answer.idNo.toUpperCase() === "POST") {
-//       //   //function of user
-//       // }
-//       // else {
-//       //   //function of user
-//       //
-//       // }
-//     });
-// }
+function displayItems(){
+  connection.query("SELECT * FROM products", function(err,res){
+
+    if (err) throw err;
+    let itemNumber = -1
+          // PRINT ALL ITEMS
+           for (var i = 0; i < res.length; i++) {
+
+             itemNumber++
+             console.log(
+               "ITEM #" + res[i].item_id + " | ",
+               "PRODUCT:" + res[i].product_name + " | ",
+               "PRICE: $" + res[i].price + " | ",
+               "ENTER " + itemNumber + " TO BUY"
+
+             );
+           }
+           //ask questions
+           start();
+
+    })
+
+}
+
+function start() {
+  inquirer
+    .prompt(
+      {
+      name: "idNo",
+      type: "input",
+      message: "Please insert ID number of product you would like to buy",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      name: "noOfItem",
+      type: "input",
+      message: "How many would you like to buy?",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    }
+  )//end prompt
+    .then(function(answer) {
+      connection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err;
+        console.log(res[parseInt(answer.idNo)].stock_quantities)
+          // compare item_no to database and if item quantity is available
+          // if (answer.noOfItem >= res[answer.item_no].stock_quantities)
+          // {
+          //   updateDatabase();
+          // }
+          // else {
+          //   console.log("Insufficient Quantity!")
+          // }
+      })
+
+    });
+}
+
+
+function updateDatabase(){
+  connection.query(
+    "INSERT INTO products SET ?",
+    {
+      stock_quantities: parseInt(answer.stock_quantities) - parseInt(answer.noOfItem)
+    },
+    function(err) {
+      if (err) throw err;
+      console.log("Please pay before your order is sent out")
+      // re-prompt the user if they want to buy more items
+      start();
+    }
+  );
+};
