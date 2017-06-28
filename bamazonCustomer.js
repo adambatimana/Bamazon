24be5cc7669ebc255f39d8869d593a894d03d13d
Adam Batimana <adambatimana@gmail.com>
2017-06-28 15:22:54 -0400

    error with quantities

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -37,7 +37,6 @@ function displayItems(){
                "PRODUCT:" + res[i].product_name + " | ",
                "PRICE: $" + res[i].price + " | ",
                "ENTER " + itemNumber + " TO BUY"
-
              );
            }
            //ask questions
@@ -48,60 +47,51 @@ function displayItems(){
 }
 
 function start() {
+  connection.query("SELECT * FROM products", function(err,res){
+
   inquirer
-    .prompt(
+    .prompt([
       {
       name: "idNo",
       type: "input",
-      message: "Please insert ID number of product you would like to buy",
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
-        }
-        return false;
-      }
+      message: "Please insert ID number of product you would like to buy"
+
     },
+
     {
       name: "noOfItem",
       type: "input",
-      message: "How many would you like to buy?",
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
-        }
-        return false;
-      }
+      message: "How many would you like to buy?"
+
     }
-  )//end prompt
+  ])//end prompt
     .then(function(answer) {
-      connection.query("SELECT * FROM products", function(err,res){
         if (err) throw err;
-        console.log(res[parseInt(answer.idNo)].stock_quantities)
+
           // compare item_no to database and if item quantity is available
-          // if (answer.noOfItem >= res[answer.item_no].stock_quantities)
-          // {
-          //   updateDatabase();
-          // }
-          // else {
-          //   console.log("Insufficient Quantity!")
-          // }
-      })
-
+          if (answer.noOfItem >= res[parseInt(answer.idNo)].stock_quantities)
+          {
+            console.log("Insufficient Quantity!")
+          }
+          else {
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              {
+                stock_quantities:res[parseInt(answer.idNo)].stock_quantities- answer.noOfItem
+              },
+              function(err) {
+                if (err) throw err;
+                console.log("Please pay before your order is sent out")
+                // re-prompt the user if they want to buy more items
+                start();
+              }
+            );
+          }
     });
-}
+})//end connection
+}//end start
 
 
-function updateDatabase(){
-  connection.query(
-    "INSERT INTO products SET ?",
-    {
-      stock_quantities: parseInt(answer.stock_quantities) - parseInt(answer.noOfItem)
-    },
-    function(err) {
-      if (err) throw err;
-      console.log("Please pay before your order is sent out")
-      // re-prompt the user if they want to buy more items
-      start();
-    }
-  );
-};
+// function updateDatabase(){
+//
+// };
