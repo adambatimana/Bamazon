d4e718cfd07cae98adf5dc149994c6cca9d1577a
Adam Batimana <adambatimana@gmail.com>
2017-06-29 10:46:04 -0400

    debugging selection menu

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,81 +27,92 @@ function displayItems(){
   connection.query("SELECT * FROM products", function(err,res){
 
     if (err) throw err;
-    let itemNumber = -1
+    // let itemNumber = -1
           // PRINT ALL ITEMS
            for (var i = 0; i < res.length; i++) {
-
-             itemNumber++
+            //  itemNumber++
              console.log(
                "ITEM #" + res[i].item_id + " | ",
                "PRODUCT:" + res[i].product_name + " | ",
-               "PRICE: $" + res[i].price + " | ",
-               "ENTER " + itemNumber + " TO BUY"
+               "PRICE: $" + res[i].price + " | "
+              //  "ENTER " + itemNumber + " TO PURCHASE"
              );
            }
            //ask questions
            start();
-
-    })
-
-}
+    })//end connection
+}//end displayItems
 
 function start() {
   connection.query("SELECT * FROM products", function(err,res){
-
   inquirer
     .prompt([
       {
       name: "idNo",
-      type: "input",
-      message: "Please insert ID number of product you would like to buy"
-
+      type: "rawlist",
+      choices: function() {
+				var choiceArray = [];
+				for(var i = 0; i < res.length; i++) {
+					choiceArray.push(res[i].item_id);
+				}
+				return choiceArray;
+			},
+      message: "Please insert the number of the product you would like to buy."
     },
-
     {
       name: "noOfItem",
       type: "input",
       message: "How many would you like to buy?"
-
     }
   ])//end prompt
     .then(function(answer) {
         if (err) throw err;
-        let answerIdno = answer.idNo
+        let choseItem;
+        let answerIdnum = answer.idNo
         let answerQuant = parseInt(answer.noOfItem)
         let stockQuant = parseInt(res[parseInt(answer.idNo)].stock_quantities)
         let updateQuant = stockQuant - answerQuant
           // compare item_no to database and if item quantity is available
-          if (answerQuant > stockQuant || stockQuant == 0)
+          if (updateQuant <= 0)
           {
             console.log("Insufficient Quantity!")
+            start();
           }
           else {
-            updateDatabase(answerIdno,updateQuant);
+            updateDatabase(answerIdnum,updateQuant);
           }//end else
+
+          if(res[i].item_id === answer.choice) {
+      			chosenItem = res[i];
+      		}
     });//end then
 
-
-
-function updateDatabase(answer,quantity){
-
+function updateDatabase(answerId,quantity){
   connection.query("UPDATE products SET ? WHERE ?",
   [
     {
       stock_quantities: quantity
     },
-    {
-      item_id: answer
+    {//update to line up itemid with anser id num
+      item_id: answerId
     }
   ],
       function(err, res) {
         if (err) throw err;
-        console.log("Please pay before your order is sent out")
+        console.log("PLEASE PAY BEFORE YOUR ORDER IS SHIPPED.")
         // re-prompt the user if they want to buy more items
         start();
       }
-
   );//end connection
-};
+};//end updateDatabase
 })//end connection
 }//end start
+
+
+function databaseQuantityLeft(){
+  connection.query("SELECT * FROM products", function(err,res){
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].stock_quantities)
+    }
+  })
+}
